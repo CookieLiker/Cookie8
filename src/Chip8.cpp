f6bad3abffbeb2ce857486f9d4d229b6e0690113
Cookie <gamerstea7@gmail.com>
2021-12-11 23:48:33 +0530

    Add Chip8 font

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -3,3 +3,12 @@
 #include <iostream>
 
 using namespace Cookie8;
+
+Chip8::Chip8()
+{
+    // Load the font into RAM
+    for (size_t i = 0x050; i <= 0x09F; i++)
+    {
+        RAM[i] = font[i - 0x050];
+    }
+}
